683fb200c07e20ab5bf78901d52bd4dca594c1c5
gwfernandez <gwfernandez@gmail.com>
2019-07-15 09:22:50 -0300

    validar extesiones de los archivos. Solo aceptar imagenes

--- src/server/routes/upload.js
+++ src/server/routes/upload.js
@@ -11,9 +11,15 @@ app.post('/upload', (req, res) => {
     if (Object.keys(req.files).length == 0) {
         return handleResponseError(400, res, 'No hay archivos a subir');
     }
+    const extensions = ['png1','jpg', 'jpeg', 'gif', 'bmp'];
 
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    // The name of the input field (i.e. "fileUpload") is used to retrieve the uploaded file
     const fileUpload = req.files.fileUpload;
+    const fileNameSplit = fileUpload.name.split('.');
+    if (fileNameSplit.length < 1) return handleResponseError(400, res, 'No puede subir una imagen sin extension');
+    const fileExtension = fileNameSplit[fileNameSplit.length-1];
+    if (!extensions.includes(fileExtension)) return handleResponseError (400, res, 'La extension del archivo no esta permitida');
+
     fileUpload.mv(`./src/uploads/${fileUpload.name}`, (err) => {
         if (err) return handleResponseError(500, res, 'Error al subir el archivo', err);
         res.json({ status: 'ok', message: 'Archivo subido correctamente' });
--- src/server/util/handlers-errors.js
+++ src/server/util/handlers-errors.js
@@ -1,11 +1,12 @@
 
 const handleResponseError = (statusCode, response, message, err) => {
-    console.error(err);
+    console.error('Error: ', message);
     let msgError = {
         status: 'error',
         message,
     };
     if (err) {
+        console.error(err);
         msgError.details = err.message;
     }
     response.status(statusCode).json(msgError);
